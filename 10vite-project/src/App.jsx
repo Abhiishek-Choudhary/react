416047c7ed312b2045b07f0b2fd7305d1aa1dcc9
RayugaLdrag <akc64016@gmail.com>
2024-07-30 11:03:50 +0530

    Redux toolkit - Todos

---
+++ 10vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './component/AddTodo'
+import Todos from './component/Todo'
+
+function App() {
+
+  return (
+    <>
+     <div>Abhishek</div>
+     <AddTodo />
+     <Todos />
+    </>
+  )
+}
+
+export default App
